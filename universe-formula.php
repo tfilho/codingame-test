c3caa100059a17ec5055fa0646d38fe9e9fa19bb
Tarcisio Filho <tarcisiobfilho@gmail.com>
2022-12-26 03:26:40 -0300

    Update universe-formula.php

--- universe-formula.php
+++ universe-formula.php
@@ -1,20 +1,19 @@
 <?php
 
 function locateUniverseFormula(){
-  
-  	$path = $_SERVER['HOME'].'/documents/';
-  	$filename = 'universe-formula';
+    $path = $_SERVER['HOME'].'/documents/';
+    $filename = 'universe-formula';
 	
     $dir   = new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS);
     $files = new RecursiveIteratorIterator($dir, RecursiveIteratorIterator::SELF_FIRST);
 
     foreach ($files as $file) {
-      if(is_file($file) && $file->getFilename() === $filename){
-        return $file->getRealPath();
-      }
+      	if(is_file($file) && $file->getFilename() === $filename){
+            return $file->getRealPath();
+      	}
     }
   
-  	return null;
+    return null;
 }
 
 $path = $_SERVER['HOME'].'/documents/';
